6b9a8f6725e463325f9cd7d12b48ad9a479e8ce2
KaiSej007 <kailyubo@gmail.com>
2023-12-01 09:44:47 +0100

    fordelt filerne for test.html

---
+++ resource/javascript/test.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    var $dishPopup = $('#dishPopup');
+
+    $('.dish-row').click(function (e) {
+        var dishName = $(this).data('name');
+        var dishPrice = parseFloat($(this).data('price'));
+        var imagePath = $(this).data('image');
+
+        $('#cart').append('<li class="list-group-item" data-price="' + dishPrice + '">' + dishName +
+            ' - $' + dishPrice.toFixed(2) + '<img src="https://static.vecteezy.com/system/resources/previews/000/630/479/non_2x/vector-trash-can-icon-symbol-illustration.jpg" class="remove-icon float-right" alt="Remove"></li>');
+
+        updateTotal();
+    });
+
+    $('#cart').on('click', '.remove-icon', function () {
+        $(this).closest('li').remove();
+        updateTotal();
+    });
+
+    $('#continueBtn').on('click', function () {
+        alert('Continue with the order! Total: $' + $('#total').text());
+    });
+
+    function updateTotal() {
+        var total = 0;
+        $('#cart li').each(function () {
+            total += parseFloat($(this).data('price'));
+        });
+        $('#total').text(total.toFixed(2));
+    }
+
+    $('.dish-row').hover(function (e) {
+        var imagePath = $(this).data('image');
+        $dishPopup.find('.dish-image').attr('src', imagePath);
+
+        var topOffset = e.pageY + 10 - $(window).scrollTop();
+        var leftOffset = e.pageX + 10;
+
+        $dishPopup.css({
+            top: topOffset,
+            left: leftOffset
+        }).show();
+    }, function () {
+        $dishPopup.hide();
+    });
+
+    $dishPopup.mouseleave(function () {
+        $dishPopup.hide();
+    });
+
+    $(window).scroll(function () {
+        $dishPopup.hide();
+    });
+});
